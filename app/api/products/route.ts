ffc872c2a1dd4cb66d3bebb6962c73eaf9020662
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2023-10-04 18:27:31 -0300

    ejercicio (1, realizado), (2, eliminar realizado, actualizar pendiente), (3, pendiente)

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -5,6 +5,9 @@ export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
 
+
+    
+
     const result = await prisma.product.create({
       data: body,
     });
@@ -23,3 +26,32 @@ export const POST = async (req: NextRequest) => {
     );
   }
 };
+
+export const DELETE = async (req: NextRequest) => {
+  try {
+
+    const body = await req.json();    
+
+    console.log("adsad");
+
+    const idFromApi = parseInt(body.id)
+
+    const result = await prisma.product.delete({
+      where:{id: idFromApi}
+    });
+    return NextResponse.json(result, {
+      status: 201,
+    });
+  } catch (err) {
+    const error = err as { message: string };
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+};
+
