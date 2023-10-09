4c38309ab850d75fd1151a0d1f7e1f4d5c20a0c4
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2023-10-09 18:52:00 -0300

    ejercicios clase lunes 2

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
-    console.log(body);
     const result = await prisma.product.create({
       data: body,
     });
