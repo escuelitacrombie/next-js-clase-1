6c5bcc5b2384944cb8c625e645d37698b01e9477
Giuliano Antonanzas <giuliano.antonanzas@4rsoluciones.com>
2023-10-25 00:37:11 -0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,17 @@ import { NextResponse, type NextRequest } from "next/server";
 import * as jose from "jose";
 
 export const config = {
-  matcher: ["/products/:path", "/products/create"],
+  matcher: ["/products/:id/", "/products/create/", "/api/products"],
 };
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const cookieStore = request.cookies;
+  const isGetOfApi =
+    request.nextUrl.pathname.startsWith("/api") && request.method === "GET";
+
+  if (isGetOfApi) return NextResponse.next();
+
   try {
     const token = cookieStore.get("token")?.value;
 
