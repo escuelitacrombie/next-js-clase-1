eda2bb850adc92eacd37ceefbea4cf6cf91ae337
Giuliano Antonanzas <giuliano.antonanzas@4rsoluciones.com>
2023-10-25 09:23:03 -0300

    remove middleware problems

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,12 @@ import { NextResponse, type NextRequest } from "next/server";
 import * as jose from "jose";
 
 export const config = {
-  matcher: ["/products/:id/", "/products/create/", "/api/products"],
+  matcher: ["/products/:id/", "/products/create/"],
 };
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const cookieStore = request.cookies;
-  const isGetOfApi =
-    request.nextUrl.pathname.startsWith("/api") && request.method === "GET";
-
-  if (isGetOfApi) return NextResponse.next();
 
   try {
     const token = cookieStore.get("token")?.value;
